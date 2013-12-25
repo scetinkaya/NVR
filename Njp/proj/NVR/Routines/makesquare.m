de8386203ce6ae62724d2882afd482a5a6fdc89c
Mehmet Serkan Apaydin <apaydin@sehir.edu.tr>
2013-12-25 11:02:09 +0200

    removed Njp

--- Njp/proj/NVR/Routines/makesquare.m
+++
@@ -1,4 +0,0 @@
-function m=makesquare(M)
-
-m = zeros(max(size(M)));
-m(1:size(M,1),1:size(M,2))=M;
--- Njp/proj/NVR/nvr/normalize.m
+++
@@ -1,4 +0,0 @@
-function [n] = normalize(v)
-% This function accepts a vector v and normalizes it to length one
-%
-n = v / (sqrt(sum (v.*v)));
