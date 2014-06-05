947601c1dbde67e54f1f27b92decfcac307b3b6e
scetinkaya <seymacetinkaya@std.sehir.edu.tr>
2014-06-05 20:40:54 +0300

    Delete Assignment.java

--- AntColonyExtraPeakAA/Assignment.java
+++
@@ -1,9 +0,0 @@
-public class Assignment {
-    public int aminoAcid;
-    public boolean feasibility;
-
-    public Assignment(int aa, boolean fsbl) {
-        aminoAcid = aa;
-        feasibility = fsbl;
-    }
-}
