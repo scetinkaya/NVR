71f15f1769b62a05be15992e52efa1ecee4b365c
scetinkaya <seymacetinkaya@std.sehir.edu.tr>
2014-06-05 20:37:52 +0300

    Create Assignment.java

---
+++ AntColonyExtraPeakAA/Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment {
+    public int aminoAcid;
+    public boolean feasibility;
+
+    public Assignment(int aa, boolean fsbl) {
+        aminoAcid = aa;
+        feasibility = fsbl;
+    }
+}
